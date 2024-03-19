2e363111e204f752f369c720d6f25e5a4d246da5
PathakRoshitha <136916400+PathakRoshitha@users.noreply.github.com>
2024-03-19 15:55:43 +0530

    Add files via upload

---
+++ HibernateUtil.java
@@ -0,0 +1,28 @@
+package Mapping_Hibernate.FirstLevelCache;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+private static final SessionFactory sessionFactory = buildSessionFactory();
+	
+	private static SessionFactory buildSessionFactory()
+	{
+		try 
+		{
+			return new
+					Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(User.class)
+			.buildSessionFactory();
+			
+		} 
+	
+		catch (Throwable ex) {
+			throw new ExceptionInInitializerError(ex);
+							}
+	
+	}
+	public static SessionFactory getSessionFactory() {
+	return sessionFactory;
+	}
+
+}
+
